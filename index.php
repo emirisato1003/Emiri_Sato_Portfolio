0c62f7063aa4461195dcd43a1c3d24e5b48e2902
emirisato1003 <e.sato@students.clark.edu>
2024-02-12 15:32:35 -0800

    Initial commit for portfolio website

---
+++ index.php
@@ -0,0 +1,322 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+
+    <!-- aos css link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="style.css">
+
+    <title>Portfolio</title>
+</head>
+
+<body>
+    <!-- header starts  -->
+    <header class="header">
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+        <a href="#home" class="logo">Portfolio.</a>
+
+
+        <nav class="navbar">
+            <a href="#home" class="active">home</a>
+            <a href="#about">about</a>
+            <a href="file/emirisato_resume.pdf">resume</a>
+            <a href="#project">project</a>
+            <a href="#contact">contact</a>
+        </nav>
+
+        <div class="follow">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+            <a href="#" class="fab fa-github"></a>
+        </div>
+    </header>
+    <!-- header ends -->
+
+    <!-- home section start -->
+    <section class="home" id="home">
+        <div class="image">
+            <img src="img/profile_image.jpg" alt="Emiri's profile picture">
+        </div>
+        <div class="content">
+            <h3>hi, i am emiri sato</h3>
+            <span>full stack web developer</span>
+            <p style="color: #c76356;">Thank you for visiting my Web site! <br> Please take the opportunity to explore and learn more about me.</p>
+            <a href="#about" class="btn">about me</a>
+        </div>
+    </section>
+    <!-- home section ends  -->
+
+    <!-- about section starts -->
+
+    <section class="about" id="about">
+
+        <h1 class="heading" data-aos="fade-up"> <span>biography</span> </h1>
+
+        <div class="bio" data-aos="fade-up">
+            <h3>
+                <span><i class="fas fa-user"></i></span>
+                &nbsp;&nbsp;emiri sato
+            </h3>
+            <h3>
+                <span><i class="fas fa-briefcase"></i></span>
+                &nbsp;&nbsp;Student at Clark College
+            </h3>
+            <h3 style="text-transform: none;">
+                <span><i class="fas fa-envelope"></i>
+                </span>&nbsp;&nbsp;emirisato.1003@gmail.com
+            </h3>
+            <h3 style="text-transform: none;">
+                <span><i class="fab fa-github"></i></span>
+                &nbsp;emirisato1003
+            </h3>
+            <h3>
+                <span><i class="fas fa-map-marker-alt"></i></span>
+                &nbsp;&nbsp;Washington, United States
+            </h3>
+        </div>
+
+        <div class="biography">
+            <p data-aos="fade-up">I enjoy creating websites that are easy and pleasant to use. I have skills in front-end and back-end programming to make smooth web solutions. I can take a project from first ideas to final website. I am eager to use my web development skills on a team to build useful new websites.</p>
+
+        </div>
+        <a href="file/emirisato_resume.pdf" class="btn" data-aos="fade-up">
+            <i class="fas fa-file-alt"></i>&nbsp;&nbsp;Resume
+        </a>
+        </div>
+
+        <!-- skills starts -->
+
+        <div class="skills" data-aos="fade-up">
+            <h1 class="heading"> <span>skills</span> </h1>
+            <ul class="language">
+                <li class="icon">
+                    <img src="icons/html-5.png" alt="HTML">
+                </li>
+                <li class="icon">
+                    <img src="icons/css-3.png" alt="CSS">
+                </li>
+                <li class="icon">
+                    <img src="icons/js.png" alt="JavaScript">
+                </li>
+                <li class="icon">
+                    <img src="icons/python.png" alt="Python">
+                </li>
+                <li class="icon">
+                    <img src="icons/php.png" alt="PHP">
+                </li>
+                <li class="icon">
+                    <img src="icons/sql-server.png" alt="SQL">
+                </li>
+                <li class="icon">
+                    <img src="icons/sass.png" alt="Sass">
+                </li>
+                <li class="icon">
+                    <img src="icons/git.png" alt="Git">
+                </li>
+                <li class="icon">
+                    <img src="icons/photoshop.png" alt="Photoshop Adobe">
+                </li>
+                <li class="icon">
+                    <img src="icons/bootstrap.png" alt="Bootstrap">
+                </li>
+                <li class="icon"> <img src="icons/wordpress.png" alt="WordPress">
+                </li>
+            </ul>
+        </div>
+        <!-- skills ends -->
+        <!-- education and experience starts  -->
+        <!-- <div class="edu-exp">
+            <h1 class="heading" data-aos="fade-up"> <span>education & experience</span></h1>
+
+            <div class="row">
+
+                <div class="box-container">
+
+                    <h3 class="title" data-aos="fade-right">education</h3>
+
+                    <div class="box" data-aos="fade-right">
+                        <span>(2022 - 2024)</span>
+                        <h3>Web Development AAT degree <br>- Clark College</h3>
+                        <p>Acquired fundamental skills in programming languages, design principles, database management, project management, and practical skills in building web applications.</p>
+                    </div>
+
+                    <div class="box" data-aos="fade-right">
+                        <span>(2019 - 2021)</span>
+                        <h3>Marketing Certificate <br>- Clark College</h3>
+                        <p>Learned essential knowledge and skills in areas such as market research branding, digital marketing, and advertising.</p>
+                    </div>
+
+                    <div class="box" data-aos="fade-right">
+                        <span>(2016 - 2022)</span>
+                        <h3>American and English Language and Literature/Letters <br>- Komazawa University, Japan</h3>
+                        <p>Adopted analytical and communication skills, comprehensive literary knowledge across diverse periods and genres, critical thinking acumen, cultural understanding, and adeptness in interpreting intricate texts.</p>
+                    </div>
+                </div> -->
+        <!-- education ends -->
+
+        <!-- experience starts -->
+
+        <!-- <div class="box-container">
+
+                    <h3 class="title" data-aos="fade-left">experience</h3>
+
+                    <div class="box" data-aos="fade-left">
+                        <span>(2022 - Present)</span>
+                        <h3>Art Gallery Assistant</h3>
+                        <p>Coordinating exhibitions, facilitating events, greeting visitors, designing promotional posters for artist talks, maintaining the gallery's website, and providing comprehensive administrative support.</p>
+                    </div>
+
+                    <div class="box" data-aos="fade-left">
+                        <span>(2020 - 2022)</span>
+                        <h3>Event Coordinator</h3>
+                        <p>Engaging in performance reviews, coordinating its sponsored events, providing leadership opportunities, organizing campus events, tracking attendance, supervising volunteers, and upholding ethical standards.</p>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+        <!-- experience ends -->
+
+    </section>
+    <!-- about section ends -->
+
+    <!-- project section starts -->
+    <section class="project" id="project">
+        <h1 class="heading" data-aos="fade-up"> <span>projects</span></h1>
+
+        <div class="box-container">
+            <div class="box" data-aos="zoom-in">
+                <a href="project.php"><img src="img/nyt_project.png" alt="New York Times News Article Project"></a>
+                <h3>New York Times News Article</h3>
+                <span>HTML | CSS | JavaScript | Bootstrap</span>
+            </div>
+
+            <div class="box" data-aos="zoom-in">
+                <a href="project.php"><img src="img/form_project_logo.png" alt="Form order Project"></a>
+                <h3>Order Form Project</h3>
+                <span>HTML | CSS | JavaScript | Bootstrap</span>
+
+            </div>
+
+
+            <div class="box" data-aos="zoom-in">
+                <a href="project.php"><img src="img/weather_forcast_project.png" alt="Weather Forecast project"></a>
+                <h3>Weather Forecast Project</h3>
+                <span>HTML | CSS | JavaScript | Bootstrap</span>
+            </div>
+
+            <div class="box" data-aos="zoom-in">
+                <a href="project.php"><img src="img/blog_project_blogpage.png" alt="Blog"></a>
+                <h3>Blog</h3>
+                <span>HTML | CSS | PHP | Bootstrap</span>
+            </div>
+
+            <div class="box" data-aos="zoom-in">
+                <a href="project.php"><img src="img/imgage_gallery_project.png" alt="Image Gallery"></a>
+                <h3>Image Gallery</h3>
+                <span>HTML | CSS | PHP | Bootstrap</span>
+            </div>
+
+            <div class="box" data-aos="zoom-in">
+                <a href="project.php"><img src="https://imgs.search.brave.com/M2v57mAxFDQqRKTAwyORr4-art3Vx1i9O3pNknQxEUk/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9wcm9k/LWRpc2NvdmVyeS5l/ZHgtY2RuLm9yZy9t/ZWRpYS9jb3Vyc2Uv/aW1hZ2UvOGY4ZTUx/MjQtMWRhYi00N2U2/LThmYTYtM2ZiZGMw/NzM4ZjBhLTQ5Nzhh/ZDkzYjFjMy5zbWFs/bC5wbmc" alt="Django Projects"></a>
+                <h3>Projects from CS50's Web Programming with Python and JavaScript</h3>
+                <span>HTML | CSS | Python | Django | Bootstrap</span>
+            </div>
+        </div>
+    </section>
+    <!-- project section ends -->
+
+    <!-- services section starts -->
+    <section class="services" id="services">
+        <h1 class="heading" data-aos="fade-up"><span>services</span></h1>
+
+        <div class="box-container">
+            <div class="box" data-aos="zoom-in">
+                <div class="logo">
+                    <i class="fas fa-code"></i>
+                </div>
+                <div class="contents">
+                    <h3>web development</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit, commodi!</p>
+                </div>
+            </div>
+
+            <div class="box" data-aos="zoom-in">
+                <div class="logo">
+                    <i class="fas fa-paint-brush"></i>
+                </div>
+                <div class="contents">
+                    <h3>Web Design</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, dolorem.</p>
+                </div>
+            </div>
+
+            <div class="box" data-aos="zoom-in">
+                <div class="logo">
+                    <i class="fab fa-wordpress"></i>
+                </div>
+                <div class="contents">
+                    <h3>WordPress Website</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, obcaecati?</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- services section ends -->
+
+    <!-- contact section starts  -->
+    <section class="contact" id="contact">
+        <h1 class="heading" data-aos="fade-up"><span>contact me</span></h1>
+        <div class="contact_info" data-aos="zoom-in">
+            <p>if you are interested in reaching out to me, feel free to email me at</p>
+            <span><strong>emirisato.1003@gmail.com</strong></span>
+        </div>
+        <!-- <form action="" method="post">
+            <div class="flex">
+                <input type="text" name="name" placeholder="enter your name" class="box" required>
+                <input type="email" name="email" placeholder="enter your email" class="box" required>
+            </div>
+
+            <input type="number" min="0" name="number" placeholder="enter your number" class="box" required>
+            <textarea name="message" class="box" required placeholder="enter your message" cols="30" rows="10"></textarea>
+            <input type="submit" value="send" name="send" class="btn">
+        </form> -->
+
+        <div class="box-container" data-aos="zoom-in">
+            <div class="box">
+                <a href="#" class="fab fa-linkedin"></a>
+                <a href="#" class="fab fa-github"></a>
+                <a href="#" class="fab fa-youtube"></a>
+            </div>
+        </div>
+
+    </section>
+    <!-- contact section ends  -->
+
+    <!-- footer section starts -->
+    <div class="credit">&copy; copyright @ <?php echo date('Y'); ?> by <span>Emiri Sato</span></div>
+    <!-- footer section ends -->
+
+    <script src="script.js"></script>
+    <!-- aos js link -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+
+    <script>
+        AOS.init({
+            duration: 800,
+            delay: 300
+        })
+    </script>
+</body>
+
+</html>
