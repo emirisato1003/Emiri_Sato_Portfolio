bbcb6f9bda496830f59683c258cc0f52cecbd4b5
emirisato1003 <e.sato@students.clark.edu>
2024-03-05 16:17:59 -0800

    adding project pages

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-let menu = document.querySelector('#menu-btn');
-let header = document.querySelector('.header');
-
-menu.onclick = () => {
-    menu.classList.toggle('fa-times');
-    header.classList.toggle('active');
-    document.body.classList.toggle('active');
-};
-
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -36,11 +27,3 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
 }
 
-
-// window.onscroll = () => {
-//     if (window.innerWidth < 991) {
-//         menu.classList.remove('fa-times');
-//         header.classList.remove('active');
-//         document.body.classList.remove('active');
-//     }
-// };
